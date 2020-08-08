a4bb9372b6a763e11ff0416e3571078a0eab80e6
Collin <collinhodgs@gmail.com>
2020-08-08 16:51:20 -0600

    Removed comments from htmlroutes.js and added const variables to apiRoutes.js

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,5 +1,6 @@
-var tableData = require("../data/tableData");
-var waitListData = require("../data/waitinglistData");
+const fs = require("fs");
+const path = require("path");
+const dbJson = require("../db/db.json");
 
 module.exports = function (app) {
   app.get("/api/notes", function (req, res) {
