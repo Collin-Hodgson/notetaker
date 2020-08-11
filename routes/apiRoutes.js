910e1e895df4f0b4dbeb83ee220508e27cdc075e
Collin <collinhodgs@gmail.com>
2020-08-10 21:20:15 -0600

    finished function to save new notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -3,6 +3,13 @@ const path = require("path");
 let dbJson = require("../db/db.json");
 let json = path.join(__dirname, "../db/db.json");
 
+function writedb(note) {
+  fs.writeFileSync(json, note, "utf8", function (err) {
+    if (err) console.log("There was an error");
+    return `Finished!`;
+  });
+}
+
 module.exports = function (app) {
   app.get("/api/notes", function (req, res) {
     res.json(dbJson);
@@ -11,10 +18,13 @@ module.exports = function (app) {
   app.post("/api/notes", function (req, res) {
     dbJson = JSON.parse(fs.readFileSync(json, "utf-8"));
     var note = {
-      title: req.body.title,
       text: req.body.text,
     };
     note.id = Math.floor(Math.random() * Date.now());
+    dbJson.push(req.body);
+    var newNote = JSON.stringify(dbJson);
+    writedb(newNote);
+    res.json(newNote);
   });
 
   app.delete("/api/notes", function (req, res) {
