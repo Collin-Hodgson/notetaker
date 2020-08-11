31e878c0c4b6401d0c63c2ef6bc05a09c926490c
Collin <collinhodgs@gmail.com>
2020-08-10 21:06:52 -0600

    started function to delete notes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,13 +1,17 @@
 const fs = require("fs");
 const path = require("path");
-const dbJson = require("../db/db.json");
+let dbJson = require("../db/db.json");
+let json = path.join(__dirname, "../db/db.json");
 
 module.exports = function (app) {
   app.get("/api/notes", function (req, res) {
-    res.json();
+    res.json(dbJson);
   });
 
   app.post("/api/notes", function (req, res) {});
 
-  app.delete("/api/notes", function (req, res) {});
+  app.delete("/api/notes", function (req, res) {
+    dbJson = JSON.parse(fs.readFileSync(json, "utf-8"));
+    let id = req.params.id;
+  });
 };
